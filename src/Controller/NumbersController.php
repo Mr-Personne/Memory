860aff2a348cb8c5b2f306e7a09f563ca779b452
Mr-Personne <h.sacha07@gmail.com>
2020-02-12 15:44:23 +0000

    testing if form is valid and redirection

--- src/Controller/NumbersController.php
+++ src/Controller/NumbersController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Time;
+use App\Form\SetupNumberType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/numbers")
@@ -23,10 +26,31 @@ class NumbersController extends AbstractController
     /**
      * @Route("/setup", name="numbers_setup")
      */
-    public function setup()
+    public function setup(Request $request)
     {
+        
+        // $time = new Time();
+        // $time->setMinutes(5);
+        // $time->setSecondes(0);
+
+        $form = $this->createForm(SetupNumberType::class);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            // $task = $form->getData();
+    
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+    
+            return $this->redirectToRoute('numbers_memorise');
+        }
+
         return $this->render('numbers/setup.html.twig', [
             'controller_name' => 'Numbers Setup',
+            'form' => $form->createView(),
         ]);
     }
 
