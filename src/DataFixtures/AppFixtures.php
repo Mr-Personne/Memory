9b395440feec9d5e55dcf9acad41f1a2e604f542
Mr-Personne <h.sacha07@gmail.com>
2020-03-08 12:14:36 +0000

    added faker fixtures sets

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use Faker;
+use App\Entity\Person;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +11,21 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // On configure dans quelles langues nous voulons nos données
+        $faker = Faker\Factory::create('fr_FR');
+
+        // on créé 10 personnes
+        for ($i = 0; $i < 10; $i++) {
+            $person = new Person();
+            $person->setFirstName($faker->name);
+            $person->setLastName($faker->name);
+            $person->setAddress($faker->streetAddress);
+            $person->setTown($faker->city);
+            $person->setPostalCode($faker->postcode);
+            $person->setAge($faker->text);
+            $person->setEmail($faker->email);
+            $manager->persist($person);
+        }
 
         $manager->flush();
     }
