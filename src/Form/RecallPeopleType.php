4643cea38105ca9a4104a27581da6183479fa2e4
Mr-Personne <h.sacha07@gmail.com>
2020-03-13 21:08:53 +0000

    testing eventlistener

--- src/Form/RecallPeopleType.php
+++ src/Form/RecallPeopleType.php
@@ -3,13 +3,24 @@
 namespace App\Form;
 
 use App\Entity\Person;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class RecallPeopleType extends AbstractType
 {
+    private function getPeopleQantity(): ?string
+    {
+        $session = new Session();
+        return $session->get('generatedPeople');
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -21,7 +32,19 @@ class RecallPeopleType extends AbstractType
             ->add('age')
             // ->add('birthDay')
             ->add('job')
-        ;
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                // $product = $event->getData();
+                $peopleQuantity = $this->getPeopleQantity();
+                $form = $event->getForm();
+
+                // checks if the Product object is "new"
+                // If no data is passed to the form, the data is "null".
+                // This should be considered a new "Product"
+                if ($peopleQuantity > 1 || 1 == 1) {
+                    $form->add('TESTFIELD', TextType::class);
+                }
+            })
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
