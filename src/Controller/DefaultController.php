b893d313c3cd72458638b9b535627135b8998858
Mr-Personne <h.sacha07@gmail.com>
2020-03-18 19:55:31 +0000

    added session clear when returning to home

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -10,8 +11,9 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
+        $session->clear();
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
