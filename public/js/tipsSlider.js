1b5d9872fc48de2aa23bb338b21c0a7c245bd6fe
Mr-Personne <h.sacha07@gmail.com>
2020-05-10 09:27:27 +0000

    getting lazy... setting up tips slider responsve

--- public/js/tipsSlider.js
+++ public/js/tipsSlider.js
@@ -1,24 +1,45 @@
-var item = document.querySelectorAll(".item");
-var slider = document.querySelector(".slider");
-var next = document.getElementById("next");
-var prev = document.getElementById("prev");
-var autoplay = document.getElementById("autoplay");
+window.addEventListener("load", () => {
+  var item = document.querySelectorAll(".item");
+  var slider = document.querySelector(".slider");
+  var next = document.getElementById("next");
+  var prev = document.getElementById("prev");
+  var autoplay = document.getElementById("autoplay");
+  var screenWidth = window.innerWidth;
 
-next.addEventListener("click", function () {
-  slider.scrollLeft += 600;
-  setTimeout(() => {
-    this.setAttribute("disabled", "true");
-  }, 0);
-  setTimeout(() => {
-    this.removeAttribute("disabled", "true");
-  }, 1000);
+  if (screenWidth >= 1200) {
+    var width = 800;
+  }
+  else if (screenWidth >= 992 && screenWidth < 1200) {
+    var width = 800;
+  }
+  else if (screenWidth >= 768 && screenWidth < 992) {
+    var width = 500;
+  }
+  else if (screenWidth >= 576 && screenWidth < 768) {
+    var width = 300;
+  }
+  else if (screenWidth < 576) {
+    var width = 300;
+  }
+
+  next.addEventListener("click", function () {
+    slider.scrollLeft += width;
+    setTimeout(() => {
+      this.setAttribute("disabled", "true");
+    }, 0);
+    setTimeout(() => {
+      this.removeAttribute("disabled", "true");
+    }, 1000);
+  });
+  prev.addEventListener("click", function () {
+    slider.scrollLeft -= width;
+    setTimeout(() => {
+      this.setAttribute("disabled", "true");
+    }, 0);
+    setTimeout(() => {
+      this.removeAttribute("disabled", "true");
+    }, 1000);
+  });
+
 });
-prev.addEventListener("click", function () {
-  slider.scrollLeft -= 600;
-  setTimeout(() => {
-    this.setAttribute("disabled", "true");
-  }, 0);
-  setTimeout(() => {
-    this.removeAttribute("disabled", "true");
-  }, 1000);
-});
+
